0d186045aa0a7c9ebedb69407dcac5a613830bc6
Rinchen Tempa Bhutia <86999338+rinchentb26@users.noreply.github.com>
2022-02-21 21:29:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-#ascii art for rock, paper and scissors
+# ascii art for rock, paper and scissors
 rock = '''
     _______
 ---'   ____)
@@ -26,29 +26,46 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-#inserting these strings onto a list
-art=[rock,paper,scissors]
+# inserting these strings onto a list
+art = [rock, paper, scissors]
 
-print("Let's Play Rock, Paper, Scissors!")
-choice=int(input("What do you choose? Type 0 for Rock 🪨, 1 for Paper📰 2 for Scissors✂️\n"))
-if choice<0 or choice>2:
-    print("Invalid Input. You lose!")
-else:
- print(art[choice])
+print("Let's Play Rock, Paper, Scissors! First to score 3 points wins.")
+name = input("What's your name? ")
+player_score = 0
+comp_score = 0
+runProgram = True
+print("Let's play!")
+while(runProgram):
+    choice = int(
+        input("What do you choose? Type 0 for Rock 🪨, 1 for Paper📰 2 for Scissors✂️\n"))
+    if choice < 0 or choice > 2:
+        print("Invalid Input. You lose!")
+    else:
+        print(art[choice])
 
- #generate random no b/w 0 and 2 for comp's choice
- ai=random.randint(0,2)
- print(f"Computer Chose {ai}")
- print(art[ai])
+    # generate random no b/w 0 and 2 for comp's choice
+    ai = random.randint(0, 2)
+    print(f"Computer Chose {ai}")
+    print(art[ai])
 
- # print results 
- if choice==ai:
-  print("Arghh! It's a draw ¯\_( ° ⏥ ° )_/¯")
- elif choice==0 and ai==2:
-  print("You win! ( ͡❛ ⏥ ͡❛)")
- elif choice==2 and ai==0:
-  print("You lose! ¯\_( ᵔ ⏥ ᵔ )_/¯")
- elif choice>ai:
-  print("You win! ( ͡❛ ⏥ ͡❛) ")
- else:
-  print("You lose! ¯\_( ᵔ ⏥ ᵔ )_/¯")
+    # print results
+    if choice == ai:
+        print("Arghh! It's a draw ¯\_( ° ⏥ ° )_/¯")
+    elif choice == 0 and ai == 2:
+        print("You win! ( ͡❛ ⏥ ͡❛)")
+        player_score += 1
+    elif choice == 2 and ai == 0:
+        print("You lose! ¯\_( ᵔ ⏥ ᵔ )_/¯")
+        comp_score += 1
+    elif choice > ai:
+        print("You win! ( ͡❛ ⏥ ͡❛) ")
+        player_score += 1
+    else:
+        print("You lose! ¯\_( ᵔ ⏥ ᵔ )_/¯")
+        comp_score += 1
+    if player_score == 3 or comp_score == 3:
+        print("\n**🏁 Final Score ** 🏁")
+        print(f"Computer - {comp_score} {name} - {player_score}")
+        break
+    print("**🏁 Current Score ** 🏁")
+    print(f"   Computer - {comp_score} {name} - {player_score}  ")
